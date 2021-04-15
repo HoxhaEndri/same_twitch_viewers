cd2a49893f612cce270fea8b80fc3dec5491d046
boh <littleleddragon@gmail.com>
2021-04-15 12:31:44 +0200

    Function

--- STV.py
+++ STV.py
@@ -7,30 +7,30 @@ def totalviewers(data):
     names.sort()
     return names
 
+def compare(url_volpe, url_moccia):
+    x = url_volpe.json()    
+    lista_volpe = totalviewers(x)
+    y = url_moccia.json()
+    lista_moccia = totalviewers(y)
+    
+    bastardi = []
+
+    for element in lista_volpe:
+        for e in lista_moccia:
+            if element == e:
+                bastardi.append(element)
+                break
+    print(bastardi)
+
 if len(sys.argv) >= 3:
     url = "https://tmi.twitch.tv/group/user/"+sys.argv[1]+"/chatters"
     url_volpe = requests.get(url)
     url = "https://tmi.twitch.tv/group/user/"+sys.argv[2]+"/chatters"
     url_moccia = requests.get(url)
+    compare(url_volpe,url_moccia)
+    
 else:
     url_volpe  = requests.get("https://tmi.twitch.tv/group/user/volpescu/chatters")
     url_moccia = requests.get("https://tmi.twitch.tv/group/user/dariomocciatwitch/chatters")
-
-x = url_volpe.json()
-
-lista_volpe = totalviewers(x)
-
-y = url_moccia.json()
-
-lista_moccia = totalviewers(y)
-
-bastardi = []
-
-for element in lista_volpe:
-    for e in lista_moccia:
-        if element == e:
-            bastardi.append(element)
-            break
-
-print(bastardi)
+    compare(url_volpe,url_moccia)
 
